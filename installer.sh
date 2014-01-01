2f85f581895dd14d53cda58940029cf85ca361ab
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2014-01-01 18:52:57 +0100

    all wokrs

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 cd;
 touch .profile;
 curl https://raw.github.com/csicar/RandomFileSelector/master/randomfileselector.sh > .randomfileselector.sh;
+chmod +x .randomfileselector.sh;
 echo "alias random=\"~/.randomfileselector.sh\"" >> .profile;
 echo "All set to go!"
 echo;
