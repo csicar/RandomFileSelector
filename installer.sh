504ed24a991996f35f8979a907a6451515bf93f9
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2014-01-01 18:45:00 +0100

    newnew

---
+++ installer.sh
@@ -0,0 +1,7 @@
+cd;
+touch .profile;
+curl https://raw.github.com/csicar/RandomFileSelector/master/randomfileselector.sh > .randomfileselector.sh;
+echo 'alias random="~/.randomfileselector.sh"';
+echo "All set to go!"
+echo;
+echo "CsiCar"
